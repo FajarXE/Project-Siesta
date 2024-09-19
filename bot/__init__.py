a7a0dda6161bdcaf56bded8df96a45fa5d319be7
Fajar Maulana <106910046+FajarXE@users.noreply.github.com>
2024-09-19 22:54:25 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,10 +1,15 @@
 from config import Config
+import subprocess, os
 
 bot = Config.BOT_USERNAME
 
 plugins = dict(
     root="bot/modules"
 )
+
+PORT = int(os.getenv("PORT"))
+
+subprocess.Popen([f"gunicorn server:app --bind 0.0.0.0:{PORT} --worker-class gevent"], shell=True)
 
 class CMD(object):
     START = ["start", f"start@{bot}"]
