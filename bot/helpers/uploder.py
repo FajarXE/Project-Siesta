b9c2ed7ca2579e1ea5930469b989fab85bcf5379
Fajar Maulana <106910046+FajarXE@users.noreply.github.com>
2024-12-02 21:03:02 +0700

    Update uploder.py

--- bot/helpers/uploder.py
+++ bot/helpers/uploder.py
@@ -1,14 +1,11 @@
+import asyncio
 import os
+import shutil
 
 from ..settings import bot_set
 from .message import send_message, edit_message
 from .utils import *
 
-#
-#
-#  TASK HANDLER
-#
-#
 
 async def track_upload(metadata, user, disable_link=False):
     if bot_set.upload_mode == 'Local':
@@ -19,11 +16,7 @@ async def track_upload(metadata, user, disable_link=False):
         rclone_link, index_link = await rclone_upload(user, metadata['filepath'])
         if not disable_link:
             await post_simple_message(user, metadata, rclone_link, index_link)
-
-    try:
-        os.remove(metadata['filepath'])
-    except FileNotFoundError:
-        pass
+    await remove_file_async(metadata['filepath'])
 
 
 async def album_upload(metadata, user):
@@ -31,22 +24,23 @@ async def album_upload(metadata, user):
         await local_upload(metadata, user)
     elif bot_set.upload_mode == 'Telegram':
         if bot_set.album_zip:
-            for item in metadata['folderpath']:
-                await send_message(user,item,'doc', 
-                    caption=await create_simple_text(metadata, user)
-                )
+            tasks = [
+                send_message(user, item, 'doc', caption=await create_simple_text(metadata, user))
+                for item in metadata['folderpath']
+            ]
+            await asyncio.gather(*tasks)
         else:
             await batch_telegram_upload(metadata, user)
     else:
         rclone_link, index_link = await rclone_upload(user, metadata['folderpath'])
         if metadata['poster_msg']:
             try:
-                await edit_art_poster(metadata, user, rclone_link, index_link, await format_string(lang.s.ALBUM_TEMPLATE, metadata, user))
+                await edit_art_poster(metadata, user, rclone_link, index_link,
+                                      await format_string(lang.s.ALBUM_TEMPLATE, metadata, user))
             except MessageNotModified:
                 pass
         else:
             await post_simple_message(user, metadata, rclone_link, index_link)
-
     await cleanup(None, metadata)
 
 
@@ -55,24 +49,24 @@ async def artist_upload(metadata, user):
         await local_upload(metadata, user)
     elif bot_set.upload_mode == 'Telegram':
         if bot_set.artist_zip:
-            for item in metadata['folderpath']:
-                await send_message(user,item,'doc', 
-                    caption=await create_simple_text(metadata, user)
-                )
+            tasks = [
+                send_message(user, item, 'doc', caption=await create_simple_text(metadata, user))
+                for item in metadata['folderpath']
+            ]
+            await asyncio.gather(*tasks)
         else:
-            pass # artist telegram uploads are handled by album fucntion
+            pass  # artist telegram uploads are handled by album function
     else:
         rclone_link, index_link = await rclone_upload(user, metadata['folderpath'])
         if metadata['poster_msg']:
             try:
-                await edit_art_poster(metadata, user, rclone_link, index_link, await format_string(lang.s.ARTIST_TEMPLATE, metadata, user))
+                await edit_art_poster(metadata, user, rclone_link, index_link,
+                                      await format_string(lang.s.ARTIST_TEMPLATE, metadata, user))
             except MessageNotModified:
                 pass
         else:
             await post_simple_message(user, metadata, rclone_link, index_link)
-
     await cleanup(None, metadata)
-
 
 
 async def playlist_upload(metadata, user):
@@ -80,10 +74,11 @@ async def playlist_upload(metadata, user):
         await local_upload(metadata, user)
     elif bot_set.upload_mode == 'Telegram':
         if bot_set.playlist_zip:
-            for item in metadata['folderpath']:
-                await send_message(user,item,'doc', 
-                    caption=await create_simple_text(metadata, user)
-                )
+            tasks = [
+                send_message(user, item, 'doc', caption=await create_simple_text(metadata, user))
+                for item in metadata['folderpath']
+            ]
+            await asyncio.gather(*tasks)
         else:
             await batch_telegram_upload(metadata, user)
     else:
@@ -91,62 +86,38 @@ async def playlist_upload(metadata, user):
             if bot_set.disable_sort_link:
                 await rclone_upload(user, f"{Config.DOWNLOAD_BASE_DIR}/{user['r_id']}/")
             else:
-                for track in metadata['tracks']:
-                    try:
-                        rclone_link, index_link = await rclone_upload(user, track['filepath'])
-                        if not bot_set.disable_sort_link:
-                            await post_simple_message(user, track, rclone_link, index_link)
-                    except ValueError: # might try to upload track which is not available
-                        pass
+                tasks = [
+                    post_simple_message(user, track, *await rclone_upload(user, track['filepath']))
+                    for track in metadata['tracks']
+                ]
+                await asyncio.gather(*tasks)
         else:
             rclone_link, index_link = await rclone_upload(user, metadata['folderpath'])
             if metadata['poster_msg']:
                 try:
-                    await edit_art_poster(metadata, user, rclone_link, index_link, await format_string(lang.s.PLAYLIST_TEMPLATE, metadata, user))
+                    await edit_art_poster(metadata, user, rclone_link, index_link,
+                                          await format_string(lang.s.PLAYLIST_TEMPLATE, metadata, user))
                 except MessageNotModified:
                     pass
             else:
                 await post_simple_message(user, metadata, rclone_link, index_link)
 
-#
-#
-#  CORE
-#
-#
 
 async def rclone_upload(user, realpath):
-    """
-    Args:
-        user: user details
-        realpath: full path to (not used for uploading)
-    Returns:
-        rclone_link, index_link
-    """
     path = f"{Config.DOWNLOAD_BASE_DIR}/{user['r_id']}/"
     cmd = f'rclone copy --config ./rclone.conf "{path}" "{Config.RCLONE_DEST}"'
     task = await asyncio.create_subprocess_shell(cmd)
     await task.wait()
-    r_link, i_link = await create_link(realpath, Config.DOWNLOAD_BASE_DIR + f"/{user['r_id']}/")
-    return r_link, i_link
+    return await create_link(realpath, Config.DOWNLOAD_BASE_DIR + f"/{user['r_id']}/")
 
 
 async def local_upload(metadata, user):
-    """
-    Copies directory to local storage and merges contents if the destination exists.
-    Args:
-        metadata: metadata dict of item
-        user: user details
-    """
     to_move = f"{Config.DOWNLOAD_BASE_DIR}/{user['r_id']}/{metadata['provider']}"
     destination = os.path.join(Config.LOCAL_STORAGE, os.path.basename(to_move))
-
-    # If the destination directory exists, merge contents
     if os.path.exists(destination):
         for item in os.listdir(to_move):
             src_item = os.path.join(to_move, item)
             dest_item = os.path.join(destination, item)
-
-            # If it's a file, copy it; if it's a directory, use copytree
             if os.path.isdir(src_item):
                 if not os.path.exists(dest_item):
                     shutil.copytree(src_item, dest_item)
@@ -154,34 +125,35 @@ async def local_upload(metadata, user):
                 shutil.copy2(src_item, dest_item)
     else:
         shutil.copytree(to_move, destination)
-    
     shutil.rmtree(to_move)
 
 
 async def telegram_upload(track, user):
-    """
-    Only upload a single track
-    Args:
-        track: track metadata
-        """
     thumb = track['filepath'].replace(track['extension'], 'jpg')
     await download_file(track['thumbnail'], thumb)
     await send_message(user, track['filepath'], 'audio', thumb=thumb, meta=track)
 
 
 async def batch_telegram_upload(metadata, user):
-    """
-    Args:
-        metadata: full metadata
-        user: user details
-    """
-    if metadata['type'] == 'album' or metadata['type'] == 'playlist':
-        for track in metadata['tracks']:
-            try:
-                await telegram_upload(track, user)
-            except FileNotFoundError:
-                pass
-    elif metadata['type'] == 'artist':
-        for album in metadata['albums']:
-            for track in album['tracks']:
-                await telegram_upload(track, user)
+    tasks = [
+        telegram_upload(track, user)
+        for album in metadata.get('albums', [])
+        for track in album['tracks']
+    ] + [
+        telegram_upload(track, user)
+        for track in metadata.get('tracks', [])
+    ]
+    await asyncio.gather(*tasks)
+
+
+async def remove_file_async(filepath):
+    try:
+        os.remove(filepath)
+    except FileNotFoundError:
+        pass
+
+
+async def cleanup(_, metadata):
+    if metadata.get('folderpath'):
+        tasks = [remove_file_async(file) for file in metadata['folderpath']]
+        await asyncio.gather(*tasks)
