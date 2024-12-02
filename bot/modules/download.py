0fdd8b19529c9e4bf3fb6418eac7065758a1090d
Fajar Maulana <106910046+FajarXE@users.noreply.github.com>
2024-12-02 20:02:19 +0700

    Update download.py

--- bot/modules/download.py
+++ bot/modules/download.py
@@ -13,24 +13,15 @@ from ..helpers.message import send_message, antiSpam, check_user, fetch_user_det
 @Client.on_message(filters.command(CMD.DOWNLOAD))
 async def download_track(c, msg: Message):
     if await check_user(msg=msg):
-        try:
-            if msg.reply_to_message:
-                link = msg.reply_to_message.text
-                reply = True
-            else:
-                link = msg.text.split(" ", maxsplit=1)[1]
-                reply = False
-        except IndexError:
+        link = await extract_link(msg)
+        if not link:
             return await send_message(msg, lang.s.ERR_NO_LINK)
 
-        if not link:
-            return await send_message(msg, lang.s.ERR_LINK_RECOGNITION)
-        
         spam = await antiSpam(msg.from_user.id, msg.chat.id)
         if not spam:
-            user = await fetch_user_details(msg, reply)
+            user = await fetch_user_details(msg)
             user['link'] = link
-            user['bot_msg'] = await send_message(msg, 'Downloading.......')
+            user['bot_msg'] = await send_message(msg, 'Downloading...')
             try:
                 await start_link(link, user)
                 await send_message(msg, lang.s.TASK_COMPLETED)
@@ -42,20 +33,35 @@ async def download_track(c, msg: Message):
                 await cleanup(user)  # deletes uploaded files
                 await antiSpam(msg.from_user.id, msg.chat.id, True)
 
+async def extract_link(msg: Message):
+    """Extracts the download link from the message."""
+    if msg.reply_to_message:
+        return msg.reply_to_message.text
+    else:
+        try:
+            return msg.text.split(" ", maxsplit=1)[1]
+        except IndexError:
+            return None
+
 async def start_link(link: str, user: dict):
-    tidal = ["https://tidal.com", "https://listen.tidal.com", "tidal.com", "listen.tidal.com"]
-    deezer = ["https://deezer.page.link", "https://deezer.com", "deezer.com", "https://www.deezer.com"]
+    """Determines the provider and initiates the download."""
+    tidal = ["https://tidal.com", "https://listen.tidal.com"]
+    deezer = ["https://deezer.page.link", "https://deezer.com"]
     qobuz = ["https://play.qobuz.com", "https://open.qobuz.com", "https://www.qobuz.com"]
     spotify = ["https://open.spotify.com"]
-    
+
     if link.startswith(tuple(tidal)):
-        return "tidal"  # Handle Tidal download here
+        user['provider'] = 'Tidal'
+        # Handle Tidal download here (implement the function accordingly)
     elif link.startswith(tuple(deezer)):
-        return "deezer"  # Handle Deezer download here
+        user['provider'] = 'Deezer'
+        # Handle Deezer download here (implement the function accordingly)
     elif link.startswith(tuple(qobuz)):
         user['provider'] = 'Qobuz'
         await start_qobuz(link, user)  # Ensure this function is implemented correctly
     elif link.startswith(tuple(spotify)):
-        return 'spotify'  # Handle Spotify download here
+        user['provider'] = 'Spotify'
+        # Handle Spotify download here (implement the function accordingly)
     else:
-        return None  # Handle unsupported link
+        LOGGER.warning(f"Unsupported link: {link}")
+        await send_message(user['bot_msg'], lang.s.ERR_UNSUPPORTED_LINK)
