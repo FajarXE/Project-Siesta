d0325b10cd5aef8a0472f3d55de3b6422d1e1f25
Fajar Maulana <106910046+FajarXE@users.noreply.github.com>
2024-12-03 11:34:08 +0700

    Update download.py

--- bot/modules/download.py
+++ bot/modules/download.py
@@ -13,54 +13,51 @@ from ..helpers.message import send_message, antiSpam, check_user, fetch_user_det
 
 @Client.on_message(filters.command(CMD.DOWNLOAD))
 async def download_track(c, msg: Message):
-    if not await check_user(msg=msg):
-        return
-
-    link, reply = extract_link(msg)
-    if not link:
-        return await send_message(msg, lang.s.ERR_NO_LINK)
-
-    if not await antiSpam(msg.from_user.id, msg.chat.id):
-        user = await fetch_user_details(msg, reply)
-        user['link'] = link
-        user['bot_msg'] = await send_message(msg, 'Downloading.......')
-        
+    if await check_user(msg=msg):
         try:
-            await start_link(link, user)
-            await send_message(user, lang.s.TASK_COMPLETED)
-        except Exception as e:
-            LOGGER.error(e)
-        finally:
-            if user.get('bot_msg') and isinstance(user['bot_msg'], Message):
+            if msg.reply_to_message:
+                link = msg.reply_to_message.text
+                reply = True
+            else:
+                link = msg.text.split(" ", maxsplit=1)[1]
+                reply = False
+        except IndexError:
+            return await send_message(msg, lang.s.ERR_NO_LINK)
+
+        if not link:
+            return await send_message(msg, lang.s.ERR_LINK_RECOGNITION)
+        
+        spam = await antiSpam(msg.from_user.id, msg.chat.id)
+        if not spam:
+            user = await fetch_user_details(msg, reply)
+            user['link'] = link
+            user['bot_msg'] = await send_message(msg, 'Downloading.......')
+            try:
+                # Start the download process based on the link
+                await start_link(link, user)
+                await send_message(user, lang.s.TASK_COMPLETED)
+            except Exception as e:
+                LOGGER.error(f"Error during download: {e}")
+                await send_message(user, lang.s.ERROR_DURING_DOWNLOAD)  # Notify user of the error
+            finally:
                 await c.delete_messages(msg.chat.id, user['bot_msg'].id)
-            await cleanup(user)  # deletes uploaded files
-            await antiSpam(msg.from_user.id, msg.chat.id, True)
-
-
-def extract_link(msg: Message):
-    try:
-        if msg.reply_to_message:
-            return msg.reply_to_message.text, True
-        else:
-            return msg.text.split(" ", maxsplit=1)[1], False
-    except:
-        return None, False
-
+                await cleanup(user)  # deletes uploaded files
+                await antiSpam(msg.from_user.id, msg.chat.id, True)
 
 async def start_link(link: str, user: dict):
-    providers = {
-        "tidal": ["https://tidal.com", "https://listen.tidal.com", "tidal.com", "listen.tidal.com"],
-        "deezer": ["https://deezer.page.link", "https://deezer.com", "deezer.com", "https://www.deezer.com"],
-        "qobuz": ["https://play.qobuz.com", "https://open.qobuz.com", "https://www.qobuz.com"],
-        "spotify": ["https://open.spotify.com"]
-    }
+    tidal = ["https://tidal.com", "https://listen.tidal.com", "tidal.com", "listen.tidal.com"]
+    deezer = ["https://deezer.page.link", "https://deezer.com", "deezer.com", "https://www.deezer.com"]
+    qobuz = ["https://play.qobuz.com", "https://open.qobuz.com", "https://www.qobuz.com"]
+    spotify = ["https://open.spotify.com"]
 
-    for provider, urls in providers.items():
-        if link.startswith(tuple(urls)):
-            if provider == 'qobuz':
-                user['provider'] = 'Qobuz'
-                await start_qobuz(link, user)
-            else:
-                return provider
-
-    return None
+    if link.startswith(tuple(tidal)):
+        return "tidal"
+    elif link.startswith(tuple(deezer)):
+        return "deezer"
+    elif link.startswith(tuple(qobuz)):
+        user['provider'] = 'Qobuz'
+        await start_qobuz(link, user)
+    elif link.startswith(tuple(spotify)):
+        return 'spotify'
+    else:
+        return None
