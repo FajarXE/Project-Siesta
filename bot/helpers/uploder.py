50a7f07e60230a2b0a4fdee8d7ea266dbbbce334
gudmeong <privatemymail758@gmail.com>
2025-06-20 07:02:47 +0700

    Check
    Signed-off-by: gudmeong <privatemymail758@gmail.com>

--- bot/helpers/uploder.py
+++ bot/helpers/uploder.py
@@ -49,7 +49,7 @@ async def album_upload(metadata, user):
         else:
             await post_simple_message(user, metadata, rclone_link, index_link)
 
-    await cleanup(None, metadata)
+    await cleanup(user, metadata)
 
 
 async def artist_upload(metadata, user):
@@ -74,7 +74,7 @@ async def artist_upload(metadata, user):
         else:
             await post_simple_message(user, metadata, rclone_link, index_link)
 
-    await cleanup(None, metadata)
+    await cleanup(user, metadata)
 
 
 
