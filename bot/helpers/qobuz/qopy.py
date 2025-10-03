30a83c3981186b5ecff9569f7d359b7f94a4c27f
Fajar Maulana <106910046+FajarXE@users.noreply.github.com>
2025-10-03 21:24:34 +0700

    Update qopy.py
    99999

--- bot/helpers/qobuz/qopy.py
+++ bot/helpers/qobuz/qopy.py
@@ -43,21 +43,21 @@ class QoClient:
             params = {
                 "extra": "tracks",
                 "playlist_id": kwargs["id"],
-                "limit": 9999,
+                "limit": 99999,
                 "offset": kwargs["offset"],
             }
         elif epoint == "artist/get":
             params = {
                 "app_id": self.id,
                 "artist_id": kwargs["id"],
-                "limit": 9999,
+                "limit": 99999,
                 "offset": kwargs["offset"],
                 "extra": "albums",
             }
         elif epoint == "label/get":
             params = {
                 "label_id": kwargs["id"],
-                "limit": 9999,
+                "limit": 99999,
                 "offset": kwargs["offset"],
                 "extra": "albums",
             }
@@ -123,11 +123,11 @@ class QoClient:
                 j = await self.api_call(epoint, id=id, offset=offset, type=type)
             if offset == 0:
                 yield j
-                total = j[key] - 9999
+                total = j[key] - 99999
             else:
                 yield j
-                total -= 9999
-            offset += 9999
+                total -= 99999
+            offset += 99999
 
 
     async def auth(self):
