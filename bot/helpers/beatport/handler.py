a8933c9bca43b5746ef8346f32d3f0447fc3897e
bibichan2010@protonmail.com <root@localhost.localdomain>
2025-11-02 06:07:14 +0000

    fixing beatport

--- bot/helpers/beatport/handler.py
+++ bot/helpers/beatport/handler.py
@@ -20,7 +20,7 @@ from ..metadata import set_metadata
 current_directory = Path(__file__).resolve().parent
 orpheus_dir = Path(__file__).resolve().parent.parent / "OrpheusDL"
 orpheusdl_config_path = orpheus_dir / "config" / "settings.json"
-orpheusdl_dir = orpheusdl_dir / "Download"
+orpheusdl_dir = orpheus_dir / "Download"
 
 def beatport_login():
     with open(orpheusdl_config_path, "r") as f:
