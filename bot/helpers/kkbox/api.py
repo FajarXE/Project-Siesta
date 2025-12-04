8f3aeb040f0d01cec9f01e93fbe3e361df4164fe
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2025-12-04 20:31:50 +0700

    Update api.py
    Hmmm...

--- bot/helpers/kkbox/api.py
+++ bot/helpers/kkbox/api.py
@@ -119,14 +119,14 @@ class KkboxAPI:
             self.available_qualities.append('hires')
 
     def get_songs(self, ids):
-        # --- PERBAIKAN: Tambahkan 'release_date' ke fields ---
+        # --- PASTIKAN 'release_date' ADA DI SINI ---
         fields_req = 'album,release_date,artist_role,song_idx,album_photo_info,song_is_explicit,song_more_url,album_more_url,artist_more_url,genre_name,is_lyrics,audio_quality'
         
         resp = self.api_call('ds', 'v2/song', payload={
             'ids': ','.join(ids),
             'fields': fields_req
         })
-        # -----------------------------------------------------
+        # -------------------------------------------
         if resp['status']['type'] != 'OK':
             raise self.exception('Track not found')
         return resp['data']['songs']
