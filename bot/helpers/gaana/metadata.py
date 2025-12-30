f6760ff30395b34327a4da52603ccdbfad2fdc85
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2025-12-30 19:07:06 +0700

    Update metadata.py

--- bot/helpers/gaana/metadata.py
+++ bot/helpers/gaana/metadata.py
@@ -14,43 +14,42 @@ async def set_gaana_metadata(file_path, track_data, album_art_path):
         artists = track_data.get("artist", [])
         if artists:
             artist_names = [a['name'] for a in artists]
-            audio["\xa9ART"] = artist_names[0] # Primary
-            # Album Artist
+            audio["\xa9ART"] = artist_names[0] 
             audio["aART"] = artist_names[0] 
         else:
              audio["\xa9ART"] = "Unknown"
              audio["aART"] = "Unknown"
 
-        # --- Extra Tags (Sesuai gaana.py) ---
+        # --- Extra Tags (Sesuai Referensi) ---
         if track_data.get("release_date"):
             audio["\xa9day"] = track_data["release_date"]
             
-        if track_data.get("gener"): # Typo di API Gaana 'gener'
+        if track_data.get("gener"): 
             genres = [g['name'] for g in track_data["gener"]]
             if genres: audio["\xa9gen"] = genres[0]
             
         if track_data.get("isrc"):
-            audio["----:com.apple.iTunes:ISRC"] = track_data["isrc"].encode('utf-8')
+            audio["----:com.apple.iTunes:ISRC"] = bytes(track_data["isrc"], 'utf-8')
 
         if track_data.get("language"):
-            audio["----:com.apple.iTunes:LANGUAGE"] = track_data["language"].title().encode('utf-8')
+            audio["----:com.apple.iTunes:LANGUAGE"] = bytes(track_data["language"], 'utf-8')
             
+        # Label & Copyright
         label = track_data.get("label_name") or "Gaana"
         audio["cprt"] = label 
-        audio["----:TXXX:Record label"] = label.encode('utf-8')
+        audio["----:TXXX:Record label"] = bytes(label, 'utf-8')
 
         # Track Number
         if track_data.get("track_number"):
-             # Format trkn: (track_num, total_tracks)
              t_num = int(track_data["track_number"])
              t_cnt = int(track_data.get("track_count", 0))
              audio["trkn"] = [(t_num, t_cnt)]
 
-        # Explicit Rating (1 = Parent Warning)
+        # Explicit Rating
         if "parental_warning" in track_data:
              audio["rtng"] = [4 if track_data["parental_warning"] == 1 else 2]
         
-        audio['stik'] = [1] # Music Media Type
+        audio['stik'] = [1] 
 
         if album_art_path:
             with open(album_art_path, 'rb') as f:
