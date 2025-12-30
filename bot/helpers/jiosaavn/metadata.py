67e111a2a5961a153a6706b871b67d77e5a5fe8f
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2025-12-30 22:48:37 +0700

    Update metadata.py

--- bot/helpers/jiosaavn/metadata.py
+++ bot/helpers/jiosaavn/metadata.py
@@ -14,18 +14,18 @@ async def set_jiosaavn_metadata(file_path, track_data, album_art_path, lyrics=No
         audio["\xa9day"] = str(track_data.get("year", ""))
         audio["\xa9wrt"] = track_data.get("music", "")
         
-        # Advanced (Reference: jiosaavn.py)
+        # Extra Tags
         if track_data.get("label"):
             audio["----:TXXX:Record label"] = bytes(track_data["label"], 'utf-8')
             audio["cprt"] = track_data.get("copyright_text", track_data["label"])
         
         if track_data.get("language"):
             audio["----:TXXX:Language"] = bytes(track_data["language"].title(), 'utf-8')
+            audio["----:com.apple.iTunes:LANGUAGE"] = bytes(track_data["language"].title(), 'utf-8')
             
         if "explicit_content" in track_data:
             audio["rtng"] = [2 if int(track_data["explicit_content"]) == 0 else 4]
 
-        # Tag Khusus JioSaavn
         if track_data.get("singers"):
              audio["----:TXXX:Singers"] = bytes(track_data["singers"], 'utf-8')
         if track_data.get("starring"):
@@ -44,7 +44,6 @@ async def set_jiosaavn_metadata(file_path, track_data, album_art_path, lyrics=No
         audio.pop("©too", None) 
         audio.save()
         
-        # Get Duration
         audio = MP4(file_path)
         duration = int(audio.info.length)
 
