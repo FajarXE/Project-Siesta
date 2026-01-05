878e715f9f0ad8a69f833814ad5af9d7e1354367
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-01-05 14:48:29 +0700

    Update __main__.py
    Add bandcamp support

--- bot/__main__.py
+++ bot/__main__.py
@@ -48,6 +48,7 @@ highresaudio_manager = safe_import('bot.helpers.highresaudio.manager', 'highresa
 moov_manager = safe_import('bot.helpers.moov.manager', 'moov_manager')
 jiosaavn_manager = safe_import('bot.helpers.jiosaavn.manager', 'jiosaavn_manager')
 gaana_manager = safe_import('bot.helpers.gaana.manager', 'gaana_manager')
+bandcamp_manager = safe_import('bot.helpers.bandcamp.manager', 'bandcamp_manager')
 
 
 async def load_all_user_settings_into_managers():
@@ -122,7 +123,7 @@ async def start_services():
         (napster_manager, "Napster"), (idagio_manager, "Idagio"),
         (nugs_manager, "Nugs"), (bugs_manager, "Bugs"),
         (highresaudio_manager, "HIGHRESAUDIO"), (moov_manager, "Moov"),
-        (jiosaavn_manager, "JioSaavn"), (gaana_manager, "Gaana")
+        (jiosaavn_manager, "JioSaavn"), (gaana_manager, "Gaana"), (bandcamp_manager, "Bandcamp")
     ]
 
     for mgr, name in managers:
@@ -176,7 +177,7 @@ async def shutdown_all_services():
         deezer_manager, beatport_manager, tidal_manager, kkbox_manager,
         beatsource_manager, soundcloud_manager, napster_manager, idagio_manager,
         nugs_manager, bugs_manager, highresaudio_manager, moov_manager,
-        jiosaavn_manager, gaana_manager
+        jiosaavn_manager, gaana_manager, bandcamp_manager
     ]
     for mgr in managers_list:
         if mgr and hasattr(mgr, 'shutdown'):
