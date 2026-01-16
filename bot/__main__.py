6c219301a246769fbe01023ad46bf2acb05d2af3
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-01-16 11:51:52 +0700

    Update __main__.py
    Add khinsider

--- bot/__main__.py
+++ bot/__main__.py
@@ -51,6 +51,7 @@ gaana_manager = safe_import('bot.helpers.gaana.manager', 'gaana_manager')
 bandcamp_manager = safe_import('bot.helpers.bandcamp.manager', 'bandcamp_manager')
 livephish_manager = safe_import('bot.helpers.livephish.manager', 'livephish_manager')
 beatstars_manager = safe_import('bot.helpers.beatstars.manager', 'beatstars_manager')
+khinsider_manager = safe_import('bot.helpers.khinsider.manager', 'khinsider_manager')
 
 
 async def load_all_user_settings_into_managers():
@@ -68,6 +69,7 @@ async def load_all_user_settings_into_managers():
             'bugs_qual': bugs_manager,
             'moov_qual': moov_manager,
             'livephish_qual': livephish_manager,
+            'khinsider_qual': khinsider_manager,
         }
 
         # Pastikan user_data sudah terisi
@@ -126,7 +128,9 @@ async def start_services():
         (napster_manager, "Napster"), (idagio_manager, "Idagio"),
         (nugs_manager, "Nugs"), (bugs_manager, "Bugs"),
         (highresaudio_manager, "HIGHRESAUDIO"), (moov_manager, "Moov"),
-        (jiosaavn_manager, "JioSaavn"), (gaana_manager, "Gaana"), (bandcamp_manager, "Bandcamp"), (livephish_manager, "LivePhish"), (beatstars_manager, "BeatStars")
+        (jiosaavn_manager, "JioSaavn"), (gaana_manager, "Gaana"), 
+        (bandcamp_manager, "Bandcamp"), (livephish_manager, "LivePhish"), 
+        (beatstars_manager, "BeatStars"), (khinsider_manager, "Khinsider")
     ]
 
     for mgr, name in managers:
@@ -180,7 +184,7 @@ async def shutdown_all_services():
         deezer_manager, beatport_manager, tidal_manager, kkbox_manager,
         beatsource_manager, soundcloud_manager, napster_manager, idagio_manager,
         nugs_manager, bugs_manager, highresaudio_manager, moov_manager,
-        jiosaavn_manager, gaana_manager, bandcamp_manager, livephish_manager, beatstars_manager
+        jiosaavn_manager, gaana_manager, bandcamp_manager, livephish_manager, beatstars_manager, khinsider_manager
     ]
     for mgr in managers_list:
         if mgr and hasattr(mgr, 'shutdown'):
